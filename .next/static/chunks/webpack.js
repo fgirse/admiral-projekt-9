4e51ba5cdc92bf4fb4aefefcbaf1d02e5fc2ecc2
frankgirse <fgirse@bluewin.ch>
2022-01-02 07:21:31 +0100

    initial 6.  commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac78770d9aa8ce8e"; }
+/******/ 		__webpack_require__.h = function() { return "5ef87d1bdb0a5cc7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
